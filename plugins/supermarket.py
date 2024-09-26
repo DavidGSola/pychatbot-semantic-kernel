39f2a4c9559738cb4eaccbb121630b42adf5f60f
David Gonzalez Sola <David.Gonzalez.Sola@unit4.com>
2024-09-26 23:36:20 +0200

    Improve plugin examples

--- plugins/supermarket.py
+++ plugins/supermarket.py
@@ -2,10 +2,17 @@ from typing import Annotated
 from semantic_kernel.functions.kernel_function_decorator import kernel_function
 
 class SupermarketPlugin:
-    @kernel_function(name="list_supermakets", description="List all supermarkets around the user")
+    @kernel_function(name="list_supermakets", description="List supermarkets with their categories")
     async def list_supermakets(
         self
-    ) -> Annotated[list[str], "List of supermarkets around the user"]:
-        "List all supermarkets around the user"
+    ) -> Annotated[list[{str, str}], "List of supermarkets and categories"]:
+        "List supermarkets with their categories"
         
-        return ["Mercadona", "Carrefour", "Coviran"]
+        return [
+            {"Derby Market", "Meat"},
+            {"Roadrunner Food Bank", "Fruit"},
+            {"Jubilee Foods", "Vegetables"},
+            {"Fry's Food Store", "Fish"},
+            {"A & J Select Market", "Sweets"},
+            {"Parsons Food", "Other"}
+        ]
